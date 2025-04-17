0ecb96fe2ba4e34be0478c9b1be123858f60db5a
mlmartin44 <mlmarti@pointpark.edu>
2025-04-17 12:36:13 -0400

    fix db.js

--- db.js
+++ db.js
@@ -34,18 +34,3 @@ module.exports = pool;
 //});
 
 //module.exports = pool;
-const Pool = require("pg").Pool;
-
-const pool = new Pool({
-    //user: "postgres",
-    user: mlmartin,
-    //host: "127.0.0.1",
-    host: "dpg-d007dpk9c44c73fan8m0-a",
-    //database: "Movies",
-    database: movies_2wmm,
-    //password: "your_new_password",
-    password: H1cH8Y268V3j6KESHUdnRXLcUhD1wUy4,
-    port: 5433
-});
-
-module.exports = pool;
