91dd523016fa8c2e5a956b3508bf9542c5f83519
mlmartin44 <mlmarti@pointpark.edu>
2025-04-21 16:50:48 -0400

    Fix: use correct search query for filtered movie title

--- src/movies/controller.js
+++ src/movies/controller.js
@@ -20,9 +20,8 @@ const getFilteredMovies = (req, res) => {
         return res.status(400).send("Missing 'title' query parameter");
     }
 
-    const query = 'SELECT * FROM "movieInfo" WHERE LOWER(title) LIKE LOWER($1)';
+    // Use the query from queries.js
     const values = [`%${title}%`];
-
     pool.query(queries.searchMoviesByTitle, values, (error, results) => {
         if (error) {
             console.error("❌ Filter query error:", error);
